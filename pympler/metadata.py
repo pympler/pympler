12889a8d9a2fd5d3042ad8b4db9dec6f2d8cdeb8
robert.schuppenies <robert.schuppenies@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-09-12 08:11:31 +0000

    Added setup script and separate metadata module. Added dist directory as target for created distribution. Adapted MANIFEST.in.

---
+++ pympler/metadata.py
@@ -0,0 +1,21 @@
+"""Project metadata.
+
+This information is used in setup.py as well as in doc/source/conf.py.
+
+"""
+project_name = 'pympler'
+version      = '0.1a1'
+url          = 'http://packages.python.org/pympler/'
+license      = 'Apache License, Version 2.0'
+
+description      = 'A development tool to measure, monitor and analyze the'
+'memory behavior of Python objects.'
+# reST formatted
+long_description = (
+"""TODO: import from pympler introduction document."""
+)
+
+author       = 'Jean Brouwers, Ludwig Haehne, Robert Schuppenies'
+author_email = 'pympler-dev@googlegroups.com'
+
+copyright    = '2008, ' + author
