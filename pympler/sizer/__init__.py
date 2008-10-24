d377867ea501c4d9dae1f5c3ce1efc02f0a9639b
MrJean1 <MrJean1@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-10-24 21:25:30 +0000

    check Python version

--- pympler/sizer/__init__.py
+++ pympler/sizer/__init__.py
@@ -1,1 +1,7 @@
+
+ # check supported Python version
+import sys
+if getattr(sys, 'hexversion', 0) < 0x2020000:
+    raise NotImplementedError('sizer requires Python 2.2 or newer')
+
 from asizeof import *
