3357cda9573032e65f663caad35c4141110399e5
robert.schuppenies <robert.schuppenies@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-10-27 20:03:09 +0000

    Silenced metadata pychecker warnings.

--- pympler/metadata.py
+++ pympler/metadata.py
@@ -9,18 +9,18 @@ import os
 project_name = 'pympler'
 version      = '0.1a1'
 url          = 'http://packages.python.org/pympler/'
-license      = 'Apache License, Version 2.0'
+license      = 'Apache License, Version 2.0' #PYCHOK valid
 author       = 'Jean Brouwers, Ludwig Haehne, Robert Schuppenies'
 author_email = 'pympler-dev@googlegroups.com'
-copyright    = '2008, ' + author
+copyright    = '2008, ' + author #PYCHOK valid
 description  = ('A development tool to measure, monitor and analyze the '
 'memory behavior of Python objects.')
 long_description = '\n'
 # location of this very module
 _modulepath = os.path.abspath(os.path.dirname(inspect.getfile(lambda foo:foo)))
-file = open(os.path.join(_modulepath, '..', 'doc', 'source', 'intro.rst'))
+introfile = open(os.path.join(_modulepath, '..', 'doc', 'source', 'intro.rst'))
 while 1:
-    line = file.readline()
+    line = introfile.readline()
     if not line:
         break
     long_description += line
