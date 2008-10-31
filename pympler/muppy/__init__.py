0e9b866a01691c4800aba6745b8204145222dd1c
robert.schuppenies <robert.schuppenies@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-10-31 09:42:16 +0000

    Made muppy's __init__.py Py3k compatible.

--- pympler/muppy/__init__.py
+++ pympler/muppy/__init__.py
@@ -4,9 +4,10 @@ Be aware that muppy will call gc.collect() whenever the object state is
 gathered to remove reference cycles.
 
 """
-import __builtin__
-if getattr(__builtin__, 'set', None) is None:
-    raise NotImplementedError('muppy requires Python 2.4 or newer')
+import sys
+version = sys.version[0] + sys.version[2]
+if int(version) < 24:
+    raise NotImplementedError('muppy requires Python 2.4 or newer.')
 
 __all__ = ['refbrowser',
            'refbrowser_gui',
