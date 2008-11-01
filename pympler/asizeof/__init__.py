90d7c1658c608aa7f86938f34434259df6012e08
MrJean1 <MrJean1@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-11-01 20:00:38 +0000

    fixed import errors with PyChecker

--- pympler/asizeof/__init__.py
+++ pympler/asizeof/__init__.py
@@ -12,20 +12,21 @@ if sys.hexversion < 0x2020000:
  # any of the modules and structure of the package
 p = sys.modules.get(_P, None)  # this package
 
- # using 'import <_M> as m' fails with Python 3.0 and
- # Python 2.x raises SyntaxError for 'import .<_M> as m'
+ # using 'from _M import *' fails with Python 3.0 and
+ # Python 2.x raises SyntaxError for 'from ._M import *'
 m = __import__(_M, globals())
-if m and m != p and p:
+if m and m != p:
      # replace this package with the (single) module
     sys.modules[_P] = m
 else:
-    raise ImportError(_P)
+    raise ImportError('import %s as %s failed' % (_M, _P))
 
 if sys.hexversion < 0x3000000:  # for Python 2.x only
-     # remove all empty sys.modules entries
-    for n, m in sys.modules.items():
-        if m is None:
-            del sys.modules[n]
+     # remove all empty sys.modules entries created
+     # as side effects of the __import__ call above
+    _P += '.'
+    for n in [n for n, m in sys.modules.iteritems()
+                    if m is None and n.startswith(_P)]:
+        del sys.modules[n]
     del n
-
-del m, _M, p, _P
+del _M, m, _P, p  # clean up
