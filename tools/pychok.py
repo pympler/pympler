38a14bf1c77934a30bcd3c5a46d3d3031c813991
MrJean1 <MrJean1@9e6fa867-6b55-0410-95a4-a37b91626726>
2008-11-26 21:59:15 +0000

    use subprocess and other updates

--- tools/pychok.py
+++ tools/pychok.py
@@ -2,20 +2,21 @@
 
  # Copyright, license and disclaimer are at the end of this file.
 
-# This is an enhanced version of the recipe posted earlier at
-# <http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/546532>
+ # This is an enhanced version of the recipe posted earlier at
+ # <http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/546532>
 '''
    This script %(argv0)s is a postprocessor for PyChecker, the Python
    source code checker available at 'http://pychecker.sourceforge.net'.
 
-   Usage: %(argv0)s [-no-OKd|-no] [-Cmd|-cmd <path>] [-Debug|-debug] [--] [opts] file.py ...
+   Usage: %(argv0)s  [-no-OKd|-no]  [-debug|-Debug]  [-cmd|-Cmd <path>]
+                    [--|---]  [<opts>]  file.py ...
 
-   where 'opts' are PyChecker options and 'file.py' and any following
+   where <opts> are PyChecker options and  file.py and any following
    command line arguments are Python source file names.
 
-   First, PyChecker is run on the 'file.py' and subsequent arguments
-   using command %(cmd)r.  The output from PyChecker is split into
-   two sets of warning messages, OK'd and regular warning messages.
+   First, run PyChecker on  file.py and any subsequent arguments using
+   command %(cmd)r.  Then, split the output from PyChecker into two
+   sets of warning messages, OK'd and regular warning messages.
 
    A warning message is considered OK'd if the Python source line
    causing the warning message ends with a comment  #PYCHOK ... or
@@ -25,17 +26,17 @@
    -no-OKd or -no.
 
    Make sure that %(cmd)r is in your search PATH or specify the
-   path to PyChecker with option -Cmd or -cmd or adjust the source
-   code of this file %(argv0)s.
+   path to PyChecker with option -cmd or -Cmd (or adjust the source
+   code of this file %(argv0)s).
 
-   Option -Debug or -debug produces additional output from %(argv0)s.
+   Option -debug or -Debug produces additional %(argv0)s output.
 
    Beware of potential overlap between these and PyChecker option
    names and abbreviations.  PyChecker options --debug and --quiet
    are honored.
 
-   Tested with PyChecker 0.8.17 using Python 2.3.4, 2.4.4 or 2.5.1 on
-   RHEL 3u7, CentOS 4.6, MacOS X Tiger (Intel) and Solaris 10.
+   Tested with PyChecker 0.8.17 using Python 2.3.4, 2.4.4, 2.5.2 or
+   2.6 on RHEL 3u7, CentOS 4.6, MacOS X Tiger (Intel) and Solaris 10.
 
 ---
 *) Both #PYCHOK and #PYCHECKER are all upper case and there must be
@@ -43,9 +44,26 @@
    the Python source line.
 '''
 
-__version__ = '2.7 (Oct 26, 2008)'
+__version__ = '2.9 (Nov 26, 2008)'
+__all__     = ('Processor', 'main')
 
 import os, sys
+
+ # defaults
+_argv0 =  sys.argv[0]
+_Cmd   = 'pychecker --limit 0'  # PyChecker 0.8.17
+_Debug =  False  # print debug info
+_OKd   =  True   # print OK'd warnings
+_Out   =  sys.stdout  # output file
+
+try:
+    from subprocess import Popen, PIPE
+     # see <http://docs.python.org/library/subprocess.html#replacing-os-popen>
+    def _popen(cmd, *unused):  # mode='r' only
+        return Popen(cmd, shell=True, stdout=PIPE).stdout
+
+except ImportError:  # no subprocess
+    _popen = os.popen
 
 def _printf(fmt, *args, **kwds):
     '''Formatted print.
@@ -61,30 +79,26 @@ class Processor(object):
     '''Processor to handle suppression of OK'd PyChecker
        warning messages, marked as such in source code.
     '''
-    _name = ''  # source file name
+    OKs = ('#PYCHOK ', '#PYCHECKER ')  # source markers
+
     _code = []  # source code lines
     _dirs = ()  # source file directories
-    _dbg  = False  # debug print
-    _out  = True  # not--quiet
-    _OKd  = True  # print OK'd warnings
+    _name = ''  # source file name
 
-    OKs   = ('#PYCHOK ', '#PYCHECKER ')  # source markers
+    _debug = _Debug  # print debug output
+    _OKd   = _OKd    # print OK'd warnings
+    _out   = _Out    # output file, None for quiet
 
-    def __init__(self, OKd=True, dbg=False):
-        self._OKd = OKd
-        self._dbg = dbg
+    def __init__(self, OKd=_OKd, debug=_Debug, out=_Out):
+        self._debug = debug
+        self._OKd   = OKd
+        self._out   = out
 
     def debugf(self, fmt, *args):
         '''Debug print.
         '''
-        if self._dbg:
-            _printf('Debug: %s ' + fmt, sys.argv[0], *args)
-
-    def printf(self, fmt, *args):
-        '''Quiet print.
-        '''
-        if self._out:
-            _printf(fmt, *args)
+        if self._debug:
+            self.printf('Debug: %s ' + fmt, _argv0, *args)
 
     def dirs(self, *args):
         '''Get all source directories.
@@ -94,9 +108,9 @@ class Processor(object):
             if f.startswith('-'):
                  # some PyChecker options
                 if f in ('--quiet', '-Q'):
-                   self._out = False
+                   self._out = None
                 elif f in ('--debug', '-d'):
-                   self._dbg = True
+                   self._debug = True
             else:
                 n = max(f.count(os.path.sep), 1)
                 d = os.path.realpath(f)
@@ -116,7 +130,7 @@ class Processor(object):
         '''
         self._code = []
         self._name = name
-        if name.endswith('.py'):
+        if name[-3:].lower() == '.py':
             self.debugf('looking for file: %s', name)
             if os.path.isabs(name):
                 ##assert(os.path.join('', name) == name)
@@ -129,8 +143,8 @@ class Processor(object):
                     f = open(t, 'r')
                     s = f.readlines()
                     f.close()
-                    self.debugf('found file: %s (%s lines)', t, len(s))
                     self._code = s
+                    self.debugf('found file: %s (%s lines)', t, len(s))
                     break
                 except (IOError, OSError, EOFError):
                     pass
@@ -152,6 +166,56 @@ class Processor(object):
             self.debugf('no line %s in file: %s', line, self._name)
         return ''  # not OK'd, not found, etc.
 
+    def printf(self, fmt, *args):
+        '''Formatted print.
+        '''
+        if self._out:
+            if args:
+                t = fmt % args
+            else:
+                t = fmt
+            self._out.write(t + os.linesep)
+
+    def process(self, output):
+        '''Split PyChecker output in OK'd
+           and other warning messages.
+        '''
+        if self._OKd:
+            self.printf('Splitting ...')
+        if self._debug:
+            self.debugf('source directories ...')
+            for t in enumerate(self._dirs):
+                self.printf('%5d: %r', *t)
+
+        mt = []  # list of 2-tuples (message, OK'd)
+        n = t = 0  # number of warnings, not OK'd and total
+        for m in output:  # process each output line
+            m = m.rstrip()
+            if m:  # only non-blank lines
+                ok, s = '', m.split(':', 2)
+                if len(s) > 2:  # file name, line number, rest
+                    ok = self.isOK(s[0], s[1])
+                    if not ok:
+                        n += 1  # non-OK'd warnings
+                    t += 1  # total warnings
+                mt.append((m, ok))
+
+        if self._out:
+            self.printf('')
+            if self._OKd:  # print OK'd warnings
+                self.printf("Lines OK'd ...")
+                m = [m + ' - ' + ok for m, ok in mt if ok]
+                self.printf(os.linesep.join(m) or 'None')
+                self.printf('')
+             # print other warnings (and lines)
+            m = [m for m, ok in mt if not ok]
+            self.printf(os.linesep.join(m))
+            if t > 0 and n == 0:
+                self.printf("All %s OK'd", t)
+            self.printf('')
+
+        return n  # number of non-OK'd warnings
+
     def run(self, cmd, *args):
         '''Run PyChecker on args and return output.
         '''
@@ -160,98 +224,65 @@ class Processor(object):
          # build cmd, run it and capture output
         c = cmd + ' ' + ' '.join(args)
         self.debugf('running %r ...', c)
-        m = os.popen(c, 'r').readlines()
+        m = _popen(c, 'r').readlines()
         self.debugf('%s lines of %r output', len(m), cmd)
         return m  # output as lines
 
-    def process(self, output):
-        '''Split PyChecker output in OK'd
-           and other warning messages.
-        '''
-        if self._OKd:
-            self.printf('Splitting ...')
-        if self._dbg:
-            self.debugf('source directories ...')
-            for t in enumerate(self._dirs):
-                _printf('%5d: %r', *t)
-
-        mt = []  # list of 2-tuples (message, OK'd)
-        n = t = 0  # number of warnings, not OK'd and total
-        for m in output:  # process each output line
-            m = m.rstrip()
-            if m:  # only non-blank lines
-                s, ok =  m.split(':', 2), ''
-                if len(s) > 2:  # file name, line number, rest
-                    t += 1  # total warnings
-                    ok = self.isOK(s[0], s[1])
-                    if not ok:
-                        n += 1  # non-OK'd warnings
-                mt.append((m, ok))
-
-        if self._OKd:  # print OK'd warnings
-            self.printf('')
-            self.printf("Lines OK'd ...")
-            m = [m + ' - ' + ok for m, ok in mt if ok] 
-            if m:
-                _printf(os.linesep.join(m))
-            else:
-                self.printf('None')
-
-        m = [m for m, ok in mt if not ok] 
-        if m:  # print other warnings (and lines)
-            self.printf('')
-            _printf(os.linesep.join(m))
-            if t > 0 and n == 0:
-                self.printf("All %s OK'd", t)
-
-        self.printf('')
-        return n  # number of non-OK'd warnings
+def main(args, OKd=_OKd, debug=_Debug, cmd=_Cmd, out=_Out):
+    '''Get a postprocessor, run PyChecker on the
+       given arguments, split the output and return
+       the number of non-OK'd PyChecker warnings.
+    '''
+    p = Processor(OKd, debug, out=out)
+    w = p.run(cmd, *args)
+    return p.process(w)
 
 
 if __name__ == '__main__':
 
-    OKd, dbg = True, False
-    cmd = 'pychecker --limit 0'  # XXX for PyChecker 0.8.17
-
       # no os.EX_... in Python 2.2
-    def _usage(x=getattr(os, 'EX_USAGE', 64), cmd='pychecker'):
+    def _usage(x=getattr(os, 'EX_USAGE', 64), cmd=_Cmd):
         '''Show usage.
         '''
-        _printf(__doc__, **{'argv0': sys.argv[0], 'cmd': cmd.split()[0]})
+        _printf(__doc__, argv0=_argv0, cmd=cmd.split()[0])
         sys.exit(x)
 
-    if len(sys.argv) < 2:
+    OKd, debug, cmd = _OKd, _Debug, _Cmd
+
+    argc = len(sys.argv)
+    if argc < 2:
         _usage(cmd=cmd)
 
      # get options, avoid conflicts
      # with 'pychecker' options
-    a = 0
-    while True:
-        a += 1
-        t  = sys.argv[a]
+    i = 1
+    while i < argc:
+        t = sys.argv[i]
         if not t.startswith('-'):
             break
         elif t in ('-help', '-h'):
             _usage(0, cmd=cmd)
         elif t in ('-no-OKd', '-no'):
-            OKd = False
-        elif t in ('-Debug', '-debug'):
-            dbg = True
-        elif t in ('-Cmd', '-cmd'):
-            a += 1
-            cmd = sys.argv[a]
+            OKd = False  # don't print OK'd warnings
+        elif t in ('-debug', '-Debug'):
+            debug = True  # print debug output
+        elif t in ('-cmd', '-Cmd'):
+            i += 1
+            if i < argc:
+                cmd = sys.argv[i]  # pychecker cmd
+            else:
+                _usage(cmd=cmd)
         else:
             if t in ('--', '---'):
-                a += 1
+                i += 1
             break
+        i += 1
 
-     # get processor, run PyChecker on all
-     # remaining args and split the output
-    p = Processor(OKd, dbg)
-    w = p.run(cmd, *sys.argv[a:])
-    if p.process(w):
+    x = 0  # no warnings or all OK'd
+    if main(sys.argv[i:], OKd=OKd, debug=debug, cmd=cmd):
          # no os.EX_... in Python 2.2
-        sys.exit(getattr(os, 'EX_DATAERR', 65))
+        x = getattr(os, 'EX_DATAERR', 65)
+    sys.exit(x)
 
 
 # License file from an earlier version of this source file follows:
@@ -290,4 +321,3 @@ if __name__ == '__main__':
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 #---------------------------------------------------------------------
-
