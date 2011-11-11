b8bd29b05a300d64d820f62d1aab9882a36fffb1
lhaehne <lhaehne@9e6fa867-6b55-0410-95a4-a37b91626726>
2011-11-11 18:06:59 +0000

    default path is empty

--- pympler/__init__.py
+++ pympler/__init__.py
@@ -1,5 +1,5 @@
 import os
 
-DATA_PATH = '/home/lha/.virtualenvs/pympler_egg/share/pympler'
+DATA_PATH = ''
 if not os.path.exists(DATA_PATH):
     DATA_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
