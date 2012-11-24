2e5797270a0b961911a53baba63ce2abf02ce153
Pankrat <lhaehne@gmail.com>
2012-11-24 05:14:07 +0100

    Release notes and version update

--- pympler/metadata.py
+++ pympler/metadata.py
@@ -5,12 +5,12 @@ This information is used in setup.py as well as in doc/source/conf.py.
 """
 
 project_name = 'Pympler'
-version      = '0.2.1'
+version      = '0.2.2'
 url          = 'http://packages.python.org/Pympler/'
 license      = 'Apache License, Version 2.0' #PYCHOK valid
 author       = 'Jean Brouwers, Ludwig Haehne, Robert Schuppenies'
 author_email = 'pympler-dev@googlegroups.com'
-copyright    = '2008-2011, ' + author #PYCHOK valid
+copyright    = '2008-2012, ' + author #PYCHOK valid
 description  = ('A development tool to measure, monitor and analyze '
                 'the memory behavior of Python objects.')
 long_description = '''
