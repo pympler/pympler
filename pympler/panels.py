0549525e30c07c3a7736ce79170b0fd41a915f56
Pankrat <lhaehne@gmail.com>
2013-05-27 22:45:19 +0200

    Auto-track model instances

--- pympler/panels.py
+++ pympler/panels.py
@@ -12,22 +12,24 @@ that can be added by overriding the `DEBUG_TOOLBAR_PANELS` setting:
         'debug_toolbar.panels.version.VersionDebugPanel',
         'debug_toolbar.panels.timer.TimerDebugPanel',
         ...
-        'pympler.debug_panel.MemoryProfilerPanel',
+        'pympler.panels.MemoryPanel',
     )
 """
 
+from pympler.classtracker import ClassTracker
 from pympler.process import ProcessMemoryInfo
 from pympler.util.stringutils import pp
 
 try:
     from debug_toolbar.panels import DebugPanel
+    from django.db.models import get_models
     from django.template.loader import render_to_string
 except ImportError:
     class DebugPanel(object):
         pass
 
 
-class MemoryProfilerPanel(DebugPanel):
+class MemoryPanel(DebugPanel):
 
     name = 'pympler'
 
@@ -36,10 +38,15 @@ class MemoryProfilerPanel(DebugPanel):
     template = 'debug_toolbar/panels/timer.html'
 
     def process_request(self, request):
+        self._tracker = ClassTracker()
+        for model in get_models():
+            self._tracker.track_class(model, keep=True)
+        self._tracker.create_snapshot('before')
         self._before = ProcessMemoryInfo()
 
     def process_response(self, request, response):
         self._after = ProcessMemoryInfo()
+        self._tracker.create_snapshot('after')
 
     def title(self):
         return 'Memory'
@@ -54,6 +61,8 @@ class MemoryProfilerPanel(DebugPanel):
         pass
 
     def content(self):
+        stats = self._tracker.stats
+        stats.annotate()
         context = self.context.copy()
         rows = [('Resident set size', self._after.rss),
                 ('Virtual size', self._after.vsz),
@@ -61,5 +70,9 @@ class MemoryProfilerPanel(DebugPanel):
         rows.extend(self._after - self._before)
         rows = [(key, pp(value)) for key, value in rows]
         rows.extend(self._after.os_specific)
+        snapshot = stats.snapshot[-1]
+        for model in stats.tracked_classes:
+            size = snapshot.classes.get(model, {}).get('sum', 0)
+            rows.append((model.__name__, size))
         context.update({'rows': rows})
         return render_to_string(self.template, context)
