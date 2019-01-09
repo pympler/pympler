05d90b85d2f58a52fae47d3ff27b7768f09c1ecc
mrJean1 <22154337+mrJean1@users.noreply.github.com>
2019-01-09 13:07:09 -0500

    Updated warning and __version__.

--- pympler/asizeof.py
+++ pympler/asizeof.py
@@ -203,7 +203,7 @@ import weakref as Weakref
 __all__ = ['adict', 'asized', 'asizeof', 'asizesof',
            'Asized', 'Asizer',  # classes
            'basicsize', 'flatsize', 'itemsize', 'leng', 'refs']
-__version__ = '18.09.12'
+__version__ = '19.01.09'
 
 # Any classes or types in modules listed in _builtin_modules are
 # considered built-in and ignored by default, as built-in functions
@@ -720,8 +720,8 @@ def _dict_refs(obj, named):
             for k, v in _items(obj):
                 yield k
                 yield v
-    except (ReferenceError, TypeError):
-        warnings.warn("Reference error iterating '%s'" % (_classof(obj),))
+    except (ReferenceError, TypeError) as x:
+        warnings.warn("While iterating '%s': %r" % (_classof(obj), x))
 
 
 def _enum_refs(obj, named):
