3eeed3144605c86f7d31b59ddb9612677fe2c1f9
Pankrat <lhaehne@gmail.com>
2019-11-12 20:27:50 +0100

    Update changelog and version

--- pympler/metadata.py
+++ pympler/metadata.py
@@ -5,7 +5,7 @@ This information is used in setup.py as well as in doc/source/conf.py.
 """
 
 project_name = 'Pympler'
-version      = '0.7'
+version      = '0.8'
 url          = 'https://github.com/pympler/pympler'
 license      = 'Apache License, Version 2.0'
 author       = 'Jean Brouwers, Ludwig Haehne, Robert Schuppenies'
@@ -31,6 +31,6 @@ web profiling frontend exposes process statistics, garbage
 visualisation and class tracker statistics.
 
 Pympler is written entirely in Python, with no dependencies to
-external libraries. It has been tested with Python 2.7, 3.4, 3.5, 3.6, 3.7, 3.8
-on Linux, Windows and MacOS X.
+external libraries. It has been tested with Python 2.7, 3.4, 3.5, 3.6,
+3.7, 3.8 on Linux, Windows and MacOS X.
 '''
